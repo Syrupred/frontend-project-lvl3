d3718445cc7d121194f20beb07efca487c4652b7
Ирина Харитонова <kuvirinaaa@gmail.com>
2022-04-20 19:58:58 +0300

    fix webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,17 @@ import path, { dirname } from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const fileNamePath = fileURLToPath(import.meta.url);
+const dirNamePath = dirname(fileNamePath);
 
 export default {
   mode: process.env.NODE_ENV || 'development',
   entry: {
-    main: path.resolve(__dirname, './src/index.js'),
+    main: path.resolve(dirNamePath, './src/index.js'),
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(dirNamePath, './dist'),
   },
   module: {
     rules: [
